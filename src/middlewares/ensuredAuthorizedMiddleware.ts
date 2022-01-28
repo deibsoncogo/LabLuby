a263d2ff681deb3c3586f8d6aaaefe26b3859d3d
Deibson Cogo <deibsoncogo@outlook.com>
2022-01-28 11:05:08 -0300

    Update ensuredAuthorizedMiddleware, correção nas regras

--- src/middlewares/ensuredAuthorizedMiddleware.ts
+++ src/middlewares/ensuredAuthorizedMiddleware.ts
@@ -19,24 +19,25 @@ export async function EnsuredAuthorizedMiddleware(
 
   const [, token] = authHeader.split(" ");
 
-  const tokenVerify = verify(token, "fa5473530e4d1a5a1e1eb53d2fedb10c") as IToken;
+  try {
+    const tokenVerify = verify(token, "fa5473530e4d1a5a1e1eb53d2fedb10c") as IToken;
 
-  if (!tokenVerify) {
+    const employeeRepository = new EmployeeRepository();
+
+    const existsEmployee = await employeeRepository.findOneIdEmployee(tokenVerify.sub);
+
+    if (!existsEmployee) {
+      throw new AppError("ID do funcionário do token é inválido", 401);
+    }
+
+    if (existsEmployee.off) {
+      throw new AppError("Funcionário do token está desligado", 401);
+    }
+
+    request.idEmployeeAuthorized = tokenVerify.sub;
+  } catch (error) {
     throw new AppError("Token inválido", 401);
   }
-  const employeeRepository = new EmployeeRepository();
-
-  const existsEmployee = await employeeRepository.findOneIdEmployee(tokenVerify.sub);
-
-  if (!existsEmployee) {
-    throw new AppError("ID do funcionário do token é inválido", 401);
-  }
-
-  if (existsEmployee.off) {
-    throw new AppError("Funcionário do token está desligado", 401);
-  }
-
-  request.idEmployeeAuthorized = tokenVerify.sub;
 
   return next();
 }
