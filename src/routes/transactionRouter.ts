992b7754af8f9571c35ecbf7f404ecf49f9c9c9c
Deibson Cogo <deibsoncogo@outlook.com>
2022-02-01 10:03:14 -0300

    Create transaction, criação da autenticação nas rotas de transações

--- src/routes/transactionRouter.ts
+++ src/routes/transactionRouter.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { EmployeeIsAdminMiddleware } from "../middlewares/employeeIsAdminMiddleware";
+import { EnsuredAuthorizedMiddleware } from "../middlewares/ensuredAuthorizedMiddleware";
 import { CreateTransactionController } from "../modules/transactions/useCases/createTransaction/createTransactionController";
 import { DeleteOneIdTransactionController } from "../modules/transactions/useCases/deleteOneIdTransaction/deleteOneIdTransactionController";
 import { FindAllFilterTransactionController } from "../modules/transactions/useCases/findAllFilterTransaction/findAllFilterTransactionController";
@@ -7,10 +9,13 @@ import { UpdateOneIdTransactionController } from "../modules/transactions/useCas
 
 const transactionRouter = Router();
 
+transactionRouter.use(EnsuredAuthorizedMiddleware);
 transactionRouter.post("/create", new CreateTransactionController().execute);
 transactionRouter.get("/one/:id", new FindOneIdTransactionController().handle);
 transactionRouter.get("/filter", new FindAllFilterTransactionController().handle);
 transactionRouter.put("/one/:id", new UpdateOneIdTransactionController().handle);
+
+transactionRouter.use(EmployeeIsAdminMiddleware);
 transactionRouter.delete("/one/:id", new DeleteOneIdTransactionController().handle);
 
 export { transactionRouter };
