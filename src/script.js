c2cefa9b1a77177c671972a0a4bb1c20856d123f
Deibson Cogo <deibsoncogo@outlook.com>
2022-02-26 09:37:32 -0300

    fix: single page responsiveness

---
+++ src/script.js
@@ -0,0 +1,29 @@
+(function(document) {
+  "use strict";
+
+  /**
+  let $buttonLotofacilType = document.querySelector(`[data-bt="lotofacil"]`);
+
+  $buttonLotofacilType.addEventListener("click", function(event) {
+    event.preventDefault();
+    console.log("clicou");
+
+    $buttonLotofacilType.setAttribute("class", "test");
+  });
+  */
+  // const optionsFetch = {
+  //   method: "GET",
+  //   mode: "cors",
+  //   cache: "default",
+  // };
+
+  // fetch(`https://my-json-server.typicode.com/deibsoncogo/FakeApi/lotofacil`, optionsFetch)
+  // .then((value) => {
+  //   return value.json().then((date) => {
+  //     console.log(date);
+  //   });
+  // })
+  // .catch((value) => {
+  //   return console.log(`Erro inesperado: ${value.message}`);
+  // });
+})(document);
