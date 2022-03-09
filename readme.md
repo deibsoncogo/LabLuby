8085afd8bcaf19721fda2bf4c3f7609f0864bbc2
Deibson Cogo <deibsoncogo@outlook.com>
2022-03-09 19:12:02 -0300

    feat: files created/modified in the second class creating migrations

--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
 # Adonis V5 - Luby Software - Manoel Neto
-Criação de uma aplicação com o Adonis V5 junto do Manuel Neto
+Criação de uma aplicação com o [Adonis V5](https://docs.adonisjs.com/guides/introduction) junto do Manuel Neto
--- start/routes.ts
+++ start/routes.ts
@@ -1,5 +1,10 @@
 import Route from '@ioc:Adonis/Core/Route'
+import Database from '@ioc:Adonis/Lucid/Database'
 
 Route.get('/', async () => {
   return { message: 'Hello world' }
+})
+
+Route.get('/userAll', async () => {
+  return Database.from('users').select('*')
 })
