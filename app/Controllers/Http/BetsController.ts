d3ed3b9e083df724a068927caa82e4e4f05aab48
Deibson Cogo <deibsoncogo@outlook.com>
2022-03-22 10:47:15 -0300

    ⚡️ fix: bet controller refactoring

--- app/Controllers/Http/BetsController.ts
+++ app/Controllers/Http/BetsController.ts
@@ -9,47 +9,64 @@ import User from 'App/Models/User'
 export default class BetsController {
   public async index({ response }: HttpContextContract) {
     const bets = await Bet.all()
+
     return response.status(200).json(bets)
   }
 
   public async store({ request, response }: HttpContextContract) {
     await request.validate(BetStoreValidator)
-    const data = request.only(['item', 'userId', 'gameId'])
-    const bet = await Bet.create(data)
 
-    const user = await User.findOrFail(data.userId)
+    const { item, userId, gameId } = request.all()
+    const bet = await Bet.create({ item, userId, gameId })
 
-    await Mail.send((message) => {
-      message
-        .from('noreply@provaadonisv5labluby.com')
-        .to(user.email)
-        .subject('Prova Adonis V5 LabLub - Nova aposta')
-        .htmlView('emails/new_bet', { name: user.name, bet })
-    })
+    const user = await User.findOrFail(userId)
+
+    try {
+      await Mail.send((message) => {
+        message
+          .from('contact@teste.com', 'Prova Adonis V5 LabLuby')
+          .replyTo('noreply@teste.com', 'Prova Adonis V5 LabLuby')
+          .to(user.email)
+          .subject('Nova aposta')
+          .htmlView('emails/new_bet', { name: user.name, bet })
+      })
+    } catch (error) {
+      return response.status(502).json(bet)
+    }
 
     return response.status(201).json(bet)
   }
 
   public async show({ params, request, response }: HttpContextContract) {
     await request.validate(BetIdValidator)
+
     const bet = await Bet.findOrFail(params.id)
+
     const users = await bet.related('users').query()
     const games = await bet.related('games').query()
+
     return response.status(200).json({ bet, users, games })
   }
 
   public async update({ params, request, response }: HttpContextContract) {
     await request.validate(BetUpdateValidator)
-    const data = request.only(['item', 'userId', 'gameId'])
+
+    const { item, userId, gameId } = request.all()
+
     const bet = await Bet.findOrFail(params.id)
-    await bet.merge(data).save()
+
+    await bet.merge({ item, userId, gameId }).save()
+
     return response.status(201).json(bet)
   }
 
   public async destroy({ params, request, response }: HttpContextContract) {
     await request.validate(BetIdValidator)
+
     const bet = await Bet.findOrFail(params.id)
+
     await bet.delete()
+
     response.status(204)
   }
 }
