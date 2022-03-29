332a5d96d65f954c162d5adbcc9bdb6c302d2d97
Deibson Cogo <deibsoncogo@outlook.com>
2022-03-29 10:06:57 -0300

    ✅ feat: game test creation

--- test/user.spec.ts
+++ test/user.spec.ts
@@ -3,9 +3,11 @@ import supertest from 'supertest'
 
 const baseUrl = `http://${process.env.HOST}:${process.env.PORT}`
 
+let user, token, game
+
 test.group('User', () => {
   test('It must be possible to register a new user', async () => {
-    await supertest(baseUrl)
+    user = await supertest(baseUrl)
       .post('/user')
       .send({
         name: 'Usuário Primeiro',
@@ -57,12 +59,58 @@ test.group('User', () => {
 
 test.group('Authenticate', () => {
   test('It must be possible to create a valid authentication', async () => {
-    await supertest(baseUrl)
+    token = await supertest(baseUrl)
       .post('/user/login')
       .send({
         email: 'devprimeiro@outlook.com',
         password: '11aaAA',
       })
       .expect(201)
+  })
+})
+
+test.group('Game', () => {
+  test('It should be possible to list all games', async () => {
+    await supertest(baseUrl)
+      .get('/game')
+      .set({ Authorization: `Bearer ${token.body.token.token}` })
+      .expect(200)
+  })
+
+  test('It should be possible to create a game', async () => {
+    game = await supertest(baseUrl)
+      .post('/game')
+      .set({ Authorization: `Bearer ${token.body.token.token}` })
+      .send({
+        type: 'Tipo Primeiro',
+        description: 'Descrição Primeiro',
+        range: 60,
+        price: 3.5,
+        maxNumber: 6,
+        color: '#ABCDEF',
+      })
+      .expect(201)
+  })
+
+  test('It should be possible to list a game', async () => {
+    await supertest(baseUrl)
+      .get(`/game/${game.body.id}`)
+      .set({ Authorization: `Bearer ${token.body.token.token}` })
+      .expect(200)
+  })
+
+  test('It should be possible to edit a game', async () => {
+    await supertest(baseUrl)
+      .put(`/game/${game.body.id}`)
+      .set({ Authorization: `Bearer ${token.body.token.token}` })
+      .send({ type: 'Tipo Primeiro Alterado' })
+      .expect(201)
+  })
+
+  test('It should be possible to delete a game', async () => {
+    await supertest(baseUrl)
+      .delete(`/game/${game.body.id}`)
+      .set({ Authorization: `Bearer ${token.body.token.token}` })
+      .expect(204)
   })
 })
