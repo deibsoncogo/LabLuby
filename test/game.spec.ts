410f4a1b8a7b4036d2f51563eb9171d52b9e99ed
Deibson Cogo <deibsoncogo@outlook.com>
2022-03-30 08:56:48 -0300

    ✅ fix: setting game test to work separately

--- test/game.spec.ts
+++ test/game.spec.ts
@@ -1,19 +1,59 @@
 import test from 'japa'
 import supertest from 'supertest'
+import Rule from 'App/Models/Rule'
+import User from 'App/Models/User'
 import Game from 'App/Models/Game'
 
 const baseUrl = `http://${process.env.HOST}:${process.env.PORT}`
 
-let game: Game
+test.group('Game', (group) => {
+  let rulePlay: Rule, ruleAdmin: Rule, user: User, token: any, game: Game
 
-test.group('Game', () => {
+  group.before(async () => {
+    rulePlay = new Rule()
+    rulePlay.level = 'play'
+    await rulePlay.save()
+
+    ruleAdmin = new Rule()
+    ruleAdmin.level = 'admin'
+    await ruleAdmin.save()
+
+    user = new User()
+    user.name = 'Usuário Segundo Teste'
+    user.email = 'devsegundo@outlook.com'
+    user.password = '22bbBB'
+    await user.save()
+
+    await user.related('rules').attach([ruleAdmin.id])
+
+    const responseToken = await supertest(baseUrl)
+      .post('/user/login')
+      .send({
+        email: 'devsegundo@outlook.com',
+        password: '22bbBB',
+      })
+      .expect(201)
+
+    token = responseToken.body.token
+  })
+
+  group.after(async () => {
+    await user.delete()
+    await ruleAdmin.delete()
+    await rulePlay.delete()
+  })
+
   test('It should be possible to list all games', async () => {
-    await supertest(baseUrl).get('/game').expect(200)
+    await supertest(baseUrl)
+      .get('/game')
+      .set({ Authorization: `Bearer ${token.token}` })
+      .expect(200)
   })
 
   test('It should be possible to create a game', async () => {
     const { body } = await supertest(baseUrl)
       .post('/game')
+      .set({ Authorization: `Bearer ${token.token}` })
       .send({
         type: 'Tipo Primeiro',
         description: 'Descrição Primeiro',
@@ -28,17 +68,24 @@ test.group('Game', () => {
   })
 
   test('It should be possible to list a game', async () => {
-    await supertest(baseUrl).get(`/game/${game.id}`).expect(200)
+    await supertest(baseUrl)
+      .get(`/game/${game.id}`)
+      .set({ Authorization: `Bearer ${token.token}` })
+      .expect(200)
   })
 
   test('It should be possible to edit a game', async () => {
     await supertest(baseUrl)
       .put(`/game/${game.id}`)
+      .set({ Authorization: `Bearer ${token.token}` })
       .send({ type: 'Tipo Primeiro Alterado' })
       .expect(201)
   })
 
   test('It should be possible to delete a game', async () => {
-    await supertest(baseUrl).delete(`/game/${game.id}`).expect(204)
+    await supertest(baseUrl)
+      .delete(`/game/${game.id}`)
+      .set({ Authorization: `Bearer ${token.token}` })
+      .expect(204)
   })
 })
