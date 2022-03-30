88cba035f09a1e38ccf28f50b1b056ec01caab3e
Deibson Cogo <deibsoncogo@outlook.com>
2022-03-30 08:33:45 -0300

    ✅ fix: access level test refactoring (rule)

--- test/rule.spec.ts
+++ test/rule.spec.ts
@@ -1,37 +1,38 @@
-import Rule from 'App/Models/Rule'
-import User from 'App/Models/User'
 import test from 'japa'
 import supertest from 'supertest'
+import Rule from 'App/Models/Rule'
+import User from 'App/Models/User'
 
-let user: User, token
 const baseUrl = `http://${process.env.HOST}:${process.env.PORT}`
 
 test.group('Access level (Rule)', (group) => {
+  let rule: Rule, user: User, token: any
+
   group.before(async () => {
-    const rule = new Rule()
+    rule = new Rule()
     rule.level = 'play'
     await rule.save()
 
-    const responseUser = await supertest(baseUrl)
-      .post('/user')
-      .send({
-        name: 'Usuário Primeiro',
-        email: 'devprimeiro@outlook.com',
-        password: '11aaAA',
-      })
-      .expect(201)
-
-    user = responseUser.body.user
+    user = new User()
+    user.name = 'Usuário Segundo Teste'
+    user.email = 'devsegundo@outlook.com'
+    user.password = '22bbBB'
+    await user.save()
 
     const responseToken = await supertest(baseUrl)
       .post('/user/login')
       .send({
-        email: 'devprimeiro@outlook.com',
-        password: '11aaAA',
+        email: 'devsegundo@outlook.com',
+        password: '22bbBB',
       })
       .expect(201)
 
     token = responseToken.body.token
+  })
+
+  group.after(async () => {
+    await user.delete()
+    await rule.delete()
   })
 
   test('It must be possible to create a valid access level (Rule)', async () => {
