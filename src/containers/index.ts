b30a1245d453ca1dc63dca7cb32d9590650a5c40
Deibson Cogo <deibsoncogo@outlook.com>
2022-03-31 16:30:58 -0300

    🐛 fix: restart application

--- src/containers/index.ts
+++
@@ -1,5 +0,0 @@
-// import { container } from "tsyringe";
-// import { ITesteRepository } from "../modules/teste/repositories/iTesteRepository";
-// import { TesteRepository } from "../modules/teste/repositories/testeRepository";
-
-// container.registerSingleton<ITesteRepository>("TesteRepository", TesteRepository);
