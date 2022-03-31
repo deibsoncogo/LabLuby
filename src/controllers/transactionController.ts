ee2046f54077357979e4a96ad30f9719056c3521
Deibson Cogo <deibsoncogo@outlook.com>
2022-03-31 16:23:23 -0300

    🐛 fix: creating as per documentation in NPM without a service file

--- src/controllers/transactionController.ts
+++ src/controllers/transactionController.ts
@@ -16,18 +16,29 @@ export class TransactionController {
       const producer = kafka.producer();
       const consumer = kafka.consumer({ groupId: "test-group" });
 
-      await producer.connect();
-      await producer.send({ topic: "test-topic", messages: [{ value: "Hello KafkaJS user!" }] });
-      await producer.disconnect();
+      const run = async () => {
+        await producer.connect();
+        await producer.send({
+          topic: "test-topic",
+          messages: [
+            { value: "Hello KafkaJS user!" },
+          ],
+        });
 
-      await consumer.connect();
-      await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
+        await consumer.connect();
+        await consumer.subscribe({ topic: "test-topic", fromBeginning: true });
+        await consumer.run({
+          eachMessage: async ({ topic, partition, message }) => {
+            console.log({
+              partition,
+              offset: message.offset,
+              value: message.value.toString(),
+            });
+          },
+        });
+      };
 
-      await consumer.run({
-        eachMessage: async ({ topic, partition, message }) => {
-          console.log({ value: message.value.toString() });
-        },
-      });
+      run().catch(console.error);
 
       return response.status(200).json(data);
     } catch (error) {
