56fa7bca5e6990ea95204ca07acca88dd330f6b0
Deibson Cogo <deibsoncogo@outlook.com>
2022-04-06 14:29:58 -0300

    ✨ feat: tuning and creating database settings

--- service/src/routes/index.ts
+++ service/src/routes/index.ts
@@ -2,6 +2,8 @@ import { Router } from 'express'
 
 const indexRouter = Router()
 
-indexRouter.get('/', (request, response) => response.status(200).json('Hello word'))
+indexRouter.get('/', (request, response) => response.status(200).json(
+  { message: 'Hello world do service' },
+))
 
 export { indexRouter }
