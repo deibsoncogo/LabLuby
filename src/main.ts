88ccf34aa9456a05c8f248fafcdcea1c261e1d65
Deibson Cogo <deibsoncogo@outlook.com>
2022-05-04 14:54:54 -0300

    feat: implementation seeder for production

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { ProductionSeed } from "../prisma/seeds/production.seed";
 
 async function bootstrap() {
+  await ProductionSeed();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3333);
