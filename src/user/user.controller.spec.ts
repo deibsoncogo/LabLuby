6c8864f754b4040bbbbe0364c0106a0ca7ccde13
Deibson Cogo <deibsoncogo@outlook.com>
2022-05-05 16:03:21 -0300

    fix: modification in some tests

--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,18 +1,49 @@
 import { Test, TestingModule } from "@nestjs/testing";
+import { response } from "express";
 import { UserController } from "./user.controller";
+import { UserResolver } from "./user.resolver";
+import { UserService } from "./user.service";
 
-describe("UserController", () => {
+describe("User controller", () => {
   let controller: UserController;
+  let resolver: UserResolver;
+  let service: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        UserResolver,
+        {
+          provide: UserService,
+          useValue: {
+            createUser: jest.fn(),
+            findUsers: jest.fn(),
+            findIdUser: jest.fn(),
+            updateUser: jest.fn(),
+            deleteUser: jest.fn(),
+            hasEmailAlreadyExistsUser: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
+    resolver = module.get<UserResolver>(UserResolver);
+    service = module.get<UserService>(UserService);
   });
 
-  it("should be defined", () => {
+  test("It should be possible to run the definition successfully", () => {
     expect(controller).toBeDefined();
+    expect(resolver).toBeDefined();
+    expect(service).toBeDefined();
+  });
+
+  describe("Method findUsers", () => {
+    test("It should be possible to run the method successfully", async () => {
+      const result = await controller.findUsers(response);
+
+      expect(result).toEqual("dd");
+    });
   });
 });
