a9fde560cfbdf6a8207f02b097c49cd9301e95d6
Deibson Cogo <deibsoncogo@outlook.com>
2022-05-04 22:32:50 -0300

    fix: changing foreign keys from usersRules to cascade

--- prisma/migrations/20220505012458_users_rules/migration.sql
+++ prisma/migrations/20220505012458_users_rules/migration.sql
@@ -13,7 +13,7 @@ CREATE TABLE "Users_Rules" (
 CREATE UNIQUE INDEX "Users_Rules_user_id_rule_id_key" ON "Users_Rules"("user_id", "rule_id");
 
 -- AddForeignKey
-ALTER TABLE "Users_Rules" ADD CONSTRAINT "Users_Rules_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE "Users_Rules" ADD CONSTRAINT "Users_Rules_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "Users"("id") ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "Users_Rules" ADD CONSTRAINT "Users_Rules_rule_id_fkey" FOREIGN KEY ("rule_id") REFERENCES "Rules"("id") ON DELETE RESTRICT ON UPDATE RESTRICT;
+ALTER TABLE "Users_Rules" ADD CONSTRAINT "Users_Rules_rule_id_fkey" FOREIGN KEY ("rule_id") REFERENCES "Rules"("id") ON DELETE CASCADE ON UPDATE CASCADE;
