4c0a50f6c57191113804e8a22cfc3e8979434545
Deibson Cogo <deibsoncogo@outlook.com>
2022-05-09 11:22:07 -0300

    fix: modifying the db script

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from "@nestjs/core";
-import { ProductionSeed } from "../prisma/seeds/production.seed";
 import { ValidationPipe } from "../src/validação.pipe";
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
-  await ProductionSeed();
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3333);
 }
+
 bootstrap();
