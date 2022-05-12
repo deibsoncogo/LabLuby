eaf971e0a57a1fb1a407caebedf701771e102e4b
Deibson Cogo <deibsoncogo@outlook.com>
2022-05-12 17:28:13 -0300

    feat: implementation method signUp emailValidator (class 8)

--- src/representations/controllers/signUp.controller.ts
+++ src/representations/controllers/signUp.controller.ts
@@ -1,9 +1,17 @@
+import { InvalidParamError } from "../errors/invalidParam.error"
 import { MissingParamError } from "../errors/missingParam.error"
 import { BadRequest } from "../helpers/http.helper"
 import { Controller } from "../protocols/controller"
+import { EmailValidatorProtocol } from "../protocols/emailValidator.protocol"
 import { HttpRequest, HttpResponse } from "../protocols/http"
 
 export class SignUpController implements Controller {
+  private emailValidatorProtocol: EmailValidatorProtocol
+
+  constructor (emailValidatorProtocol: EmailValidatorProtocol) {
+    this.emailValidatorProtocol = emailValidatorProtocol
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields = ["name", "email", "password", "passwordConfirmation"]
 
@@ -11,6 +19,12 @@ export class SignUpController implements Controller {
       if (!httpRequest.body[field]) {
         return BadRequest(new MissingParamError(field))
       }
+    }
+
+    const isValid = this.emailValidatorProtocol.isValid(httpRequest.body.email)
+
+    if (!isValid) {
+      return BadRequest(new InvalidParamError("email"))
     }
   }
 }
