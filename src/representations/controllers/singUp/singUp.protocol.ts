b05774abd9f71138332f0d6f85b5a41574a0761c
Deibson Cogo <deibsoncogo@outlook.com>
2022-05-18 14:26:30 -0300

    feat: content learned in class 10

---
+++ src/representations/controllers/singUp/singUp.protocol.ts
@@ -0,0 +1,4 @@
+export * from "../../protocols"
+export * from "../../protocols/emailValidator.protocol"
+export * from "../../../domains/models/account.model"
+export * from "../../../domains/useCases/addAccount.useCase"
