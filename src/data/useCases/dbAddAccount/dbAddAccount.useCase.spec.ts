f69cbcd2a1bd772f790334984740c7e56f3807e0
Deibson Cogo <deibsoncogo@outlook.com>
2022-05-18 19:27:04 -0300

    feat: content learned in class 14

--- src/data/useCases/dbAddAccount/dbAddAccount.useCase.spec.ts
+++ src/data/useCases/dbAddAccount/dbAddAccount.useCase.spec.ts
@@ -1,9 +1,10 @@
-import { Encrypter } from "../../protocols/encrypter"
 import { DbAddAccount } from "./dbAddAccount.useCase"
+import { AccountModel, AddAccountModel, AddAccountRepository, Encrypter } from "./dbAddAccount.useCase.protocol"
 
 interface SubTypes {
   sut: DbAddAccount
   encrypterStub: Encrypter
+  addAccountRepositoryStub: AddAccountRepository
 }
 
 const makeEncrypter = (): Encrypter => {
@@ -16,12 +17,30 @@ const makeEncrypter = (): Encrypter => {
   return new EncrypterStub()
 }
 
+const makeAddAccountRepository = (): AddAccountRepository => {
+  class AddAccountRepositoryStub implements AddAccountRepository {
+    async add (accountData: AddAccountModel): Promise<AccountModel> {
+      const fakeAccount = {
+        id: "valid_id",
+        name: "valid_name",
+        email: "valid_email",
+        password: "hashed_password"
+      }
+
+      return new Promise((resolve) => resolve(fakeAccount))
+    }
+  }
+
+  return new AddAccountRepositoryStub()
+}
+
 const makeSut = (): SubTypes => {
   const encrypterStub = makeEncrypter()
+  const addAccountRepositoryStub = makeAddAccountRepository()
 
-  const sut = new DbAddAccount(encrypterStub)
+  const sut = new DbAddAccount(encrypterStub, addAccountRepositoryStub)
 
-  return { sut, encrypterStub }
+  return { sut, encrypterStub, addAccountRepositoryStub }
 }
 
 describe("DbAddAccount useCase", () => {
@@ -57,5 +76,62 @@ describe("DbAddAccount useCase", () => {
     const promise = sut.add(accountData)
 
     await expect(promise).rejects.toThrow()
+  })
+
+  test("Should call AddAccountRepository with correct values", async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+
+    const addSpy = jest.spyOn(addAccountRepositoryStub, "add")
+
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password"
+    }
+
+    await sut.add(accountData)
+
+    expect(addSpy).toHaveBeenCalledWith({
+      name: "valid_name",
+      email: "valid_email",
+      password: "hashed_password"
+    })
+  })
+
+  test("Should throw if Encrypter throws", async () => {
+    const { sut, addAccountRepositoryStub } = makeSut()
+
+    jest
+      .spyOn(addAccountRepositoryStub, "add")
+      .mockReturnValueOnce(new Promise((resolve, reject) => { reject(new Error()) }))
+
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password"
+    }
+
+    const promise = sut.add(accountData)
+
+    await expect(promise).rejects.toThrow()
+  })
+
+  test("Should return an account on success", async () => {
+    const { sut } = makeSut()
+
+    const accountData = {
+      name: "valid_name",
+      email: "valid_email",
+      password: "valid_password"
+    }
+
+    const account = await sut.add(accountData)
+
+    expect(account).toEqual({
+      id: "valid_id",
+      name: "valid_name",
+      email: "valid_email",
+      password: "hashed_password"
+    })
   })
 })
--- src/data/useCases/dbAddAccount/dbAddAccount.useCase.ts
+++ src/data/useCases/dbAddAccount/dbAddAccount.useCase.ts
@@ -1,15 +1,20 @@
-import { AccountModel } from "../../../domains/models/account.model"
-import { AddAccount, AddAccountModel } from "../../../domains/useCases/addAccount.useCase"
-import { Encrypter } from "../../protocols/encrypter"
+import { AccountModel, AddAccount, AddAccountModel, AddAccountRepository, Encrypter } from "./dbAddAccount.useCase.protocol"
 
 export class DbAddAccount implements AddAccount {
   private readonly encrypter: Encrypter
+  private readonly addAccountRepository: AddAccountRepository
 
-  constructor (encrypter: Encrypter) { this.encrypter = encrypter }
+  constructor (encrypter: Encrypter, addAccountRepository: AddAccountRepository) {
+    this.encrypter = encrypter
+    this.addAccountRepository = addAccountRepository
+  }
 
-  async add (account: AddAccountModel): Promise<AccountModel> {
-    await this.encrypter.encrypt(account.password)
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
 
-    return new Promise((resolve) => { resolve(null) })
+    const account = await this.addAccountRepository
+      .add(Object.assign({}, accountData, { password: hashedPassword }))
+
+    return account
   }
 }
