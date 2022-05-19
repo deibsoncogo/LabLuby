b4f558962bef321f35c5f0c95ac12b1e2f4f505e
Deibson Cogo <deibsoncogo@outlook.com>
2022-05-19 13:33:25 -0300

    feat: content learned in class 19

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,8 +1,10 @@
 import express from "express"
 import setupMiddlewares from "./middleware"
+import setupRouters from "./router"
 
 const app = express()
 
 setupMiddlewares(app)
+setupRouters(app)
 
 export default app
