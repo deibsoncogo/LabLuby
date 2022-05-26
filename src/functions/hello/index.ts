a25e7111aef4e20761b50d7b66f53431065f2ca1
Deibson Cogo <deibsoncogo@outlook.com>
2022-05-26 10:06:58 -0300

    style: add the Eslint

--- src/functions/hello/index.ts
+++ src/functions/hello/index.ts
@@ -1,19 +1,15 @@
-import schema from './schema';
-import { handlerPath } from '@libs/handler-resolver';
+import { handlerPath } from "@libs/handler-resolver"
+import schema from "./schema"
 
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   events: [
     {
       http: {
-        method: 'post',
-        path: 'hello',
-        request: {
-          schemas: {
-            'application/json': schema,
-          },
-        },
+        method: "post",
+        path: "hello",
+        request: { schemas: { "application/json": schema } },
       },
     },
   ],
-};
+}
