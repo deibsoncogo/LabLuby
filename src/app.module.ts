d4153bf96f6ee2b0e91ec7c35b3a6e83253c67a8
Deibson Cogo <deibsoncogo@outlook.com>
2022-05-30 14:43:43 -0300

    database: implementation migrate users

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { GraphQLModule } from "@nestjs/graphql"
 import { join } from "path"
 import { AppController } from "./app.controller"
 import { AppService } from "./app.service"
+import { DatabaseModule } from "./database/database.module"
+import { UserModule } from "./user/user.module"
 
 @Module({
   imports: [
+    UserModule,
+    DatabaseModule,
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), "src/schema.gql"),
--- src/serverless.ts
+++ src/serverless.ts
@@ -1,6 +1,6 @@
 import { ValidationPipe } from "@nestjs/common"
 import { NestFactory } from "@nestjs/core"
-import serverlessExpress from "@vendia/serverless-express"
+import { configure as serverlessExpress } from "@vendia/serverless-express"
 import { Callback, Context, Handler } from "aws-lambda"
 import * as cookieParser from "cookie-parser"
 import { AppModule } from "./app.module"
