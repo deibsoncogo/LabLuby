c98e8c8d7056c81343e5ab1fb2ee8e96d321714f
Deibson Cogo <deibsoncogo@outlook.com>
2022-06-01 11:03:50 -0300

    auth: setting access levels for routes

--- msUser/src/app.controller.ts
+++ msUser/src/app.controller.ts
@@ -1,12 +1,14 @@
 import { Request, Response } from "express"
 import { Controller, Get, Req, Res } from "@nestjs/common"
 import { AppService } from "./app.service"
+import { AccessLevel, AccessLevels } from "./auth/accessLevel.decorator"
 
 @Controller("app")
 export class AppController {
   constructor(private appService: AppService) {}
 
   @Get()
+  @AccessLevel(AccessLevels.Private)
   async findApps(@Req() request: Request, @Res() response: Response): Promise<Response> {
     const token = request.headers.authorization
     return response.status(200).json(await this.appService.findApps(token))
--- msUser/src/user/user.controller.ts
+++ msUser/src/user/user.controller.ts
@@ -1,5 +1,6 @@
 import { Response } from "express"
 import { Body, Controller, Delete, Get, Param, Post, Put, Res } from "@nestjs/common"
+import { AccessLevel, AccessLevels } from "../auth/accessLevel.decorator"
 import { CreateUserDto } from "./dto/createUser.dto"
 import { IdUserDto } from "./dto/idUser.dto"
 import { UpdateUserDto } from "./dto/updateUser.dto"
@@ -10,23 +11,27 @@ export class UserController {
   constructor(private userService: UserService) {}
 
   @Post()
+  @AccessLevel(AccessLevels.Public)
   async createUser(@Body() body: CreateUserDto, @Res() response: Response): Promise<Response> {
     const { name, cpf, email, password } = body
     return response.status(201).json(await this.userService.createUser({ name, cpf, email, password }))
   }
 
   @Get()
+  @AccessLevel(AccessLevels.Private)
   async findUsers(@Res() response: Response): Promise<Response> {
     return response.status(200).json(await this.userService.findUsers())
   }
 
   @Get(":id")
+  @AccessLevel(AccessLevels.Private)
   async findIdUser(@Param() param: IdUserDto, @Res() response: Response): Promise<Response> {
     const { id } = param
     return response.status(200).json(await this.userService.findIdUser(id))
   }
 
   @Put(":id")
+  @AccessLevel(AccessLevels.Private)
   async updateUser(
     @Param() param: IdUserDto,
     @Body() body: UpdateUserDto,
@@ -38,7 +43,8 @@ export class UserController {
     return response.status(201).json(await this.userService.updateUser(id, { name, cpf, email, password }))
   }
 
-    @Delete(":id")
+  @Delete(":id")
+  @AccessLevel(AccessLevels.Private)
   async deleteUser(@Param() param: IdUserDto, @Res() response: Response): Promise<Response> {
     const { id } = param
     return response.status(205).json(await this.userService.deleteUser(id))
