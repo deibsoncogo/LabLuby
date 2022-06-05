77380e6b08bc0ed0eb2bc52b5b99cd3fba95117f
Deibson Cogo <deibsoncogo@outlook.com>
2022-06-05 16:25:01 -0300

    database: implementaion orm prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { join } from 'path'
 import { AppController } from './app.controller'
 import { AppResolver } from './app.resolver'
 import { AppService } from './app.service'
+import { DatabaseModule } from './database/database.module'
 
 @Module({
   imports: [
@@ -12,6 +13,7 @@ import { AppService } from './app.service'
       driver: ApolloDriver,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    DatabaseModule,
   ],
   providers: [AppService, AppResolver],
   controllers: [AppController],
