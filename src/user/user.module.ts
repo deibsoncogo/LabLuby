e99eab2f2bb9f394a2027c5920b49ce8bda85ee2
Deibson Cogo <deibsoncogo@outlook.com>
2022-06-06 11:42:27 -0300

    fix user: removing the http protocol (controller)

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common'
 import { DatabaseModule } from 'src/database/database.module'
-import { UserController } from './user.controller'
 import { UserResolver } from './user.resolver'
 import { UserService } from './user.service'
 
 @Module({
   imports: [DatabaseModule],
   providers: [UserService, UserResolver],
-  controllers: [UserController],
 })
 export class UserModule {}
