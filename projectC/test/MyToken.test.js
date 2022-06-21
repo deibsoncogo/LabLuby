c4130ab178f76f790304d1d92f899092907a1cd2
Deibson Cogo <deibsoncogo@outlook.com>
2022-06-21 15:19:10 -0300

    feat projectC: implementation of class 119

--- projectC/test/MyToken.test.js
+++ projectC/test/MyToken.test.js
@@ -11,11 +11,13 @@ chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
+require("dotenv").config({ path: "../.env" });
+
 contract("Token Test", async (accounts) => {
   const [deployerAccount, recipient, anotherAccount] = accounts;
 
   beforeEach(async() => {
-    this.myToken = await Token.new(1000000);
+    this.myToken = await Token.new(process.env.INITIAL_TOKENS);
   });
 
   it("All tokens should be in my account", async () => {
