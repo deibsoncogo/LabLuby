e1ca6ae470501a65b25e7085c002fbf20cd4b3e2
Deibson Cogo <deibsoncogo@outlook.com>
2022-06-22 15:13:12 -0300

    feat: implementation and ERC20

--- src/contract/TokenERC20.sol
+++ src/contract/TokenERC20.sol
@@ -13,13 +13,17 @@ contract DeibsonLabLubyToken is ERC20 {
   uint256 private _totalSupply;
   // uint8 private _decimals; // esta variável está sendo criada de forma estatica
 
-  // vai guarda o saldo das carteiras
-  mapping(address => uint256) private _balances;
+  // serve para guardar valores
+  mapping(address => uint256) private _balances; // vai guarda o saldo das carteiras
+  mapping(address => mapping(address => uint256)) private _allowances; // vai guardar permições e seus valores
+
+  // cria um evento para ser executado futuramente
+  // event Transfer(address indexed _from, address indexed _to, uint256 _value);
+  // event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
   // vai criar o valor inicial do token
-  constructor() {
-    _totalSupply = 1000000;
-    _balances[msg.sender] = _totalSupply;
+  constructor(uint256 initialSupply) ERC20("Deibson Lab Luby", "DLL") {
+    _mint(msg.sender, initialSupply);
   }
 
   // criando funções para ler as variáveis principais privadas de forma externa
@@ -38,5 +42,76 @@ contract DeibsonLabLubyToken is ERC20 {
   // criando a função que vai criar e ler a variável principal privada _decimals
   function decimals() public override pure returns(uint8) {
     return 18;
+  }
+
+  // vai retornar o saldo de uma carteira
+  function balanceOf(address _owner) public override view returns(uint256) {
+    return _balances[_owner];
+  }
+
+  // função que vai executar uma transferencia pelo dono
+  function transfer(address recipient, uint256 amount) public override returns(bool) {
+    _transfer(msg.sender, recipient, amount);
+
+    return true;
+  }
+
+  // função que vai executar uma transferencia por um terceiro
+  function transferFrom(address sender, address recipient, uint256 amount) public override returns(bool) {
+    require(_allowances[sender][msg.sender] >= amount, "ERC20: Valor maior que o autorizado");
+
+    _transfer(sender, recipient, amount);
+    _approve(sender, msg.sender, _allowances[sender][msg.sender] - amount);
+
+    return true;
+  }
+
+  // vai executar a transferencia de valor entre contas
+  function _transfer(address sender, address recipient, uint256 amount) internal override {
+    require(_balances[sender] >= amount, "ERC20: Saldo insuficiente para a tranferencia");
+
+    // unchecked certifica que os valores são do tamanho de 256 caracteres
+    unchecked { _balances[sender] -= amount; } // executa a saída do valor na carteira
+    unchecked { _balances[recipient] += amount; } // executa a entrada do valor na carteira
+
+    // emite o evento obrigatório valando que foi executado a transferencia
+    emit Transfer(sender, recipient, amount);
+  }
+
+  // função que vai definir uma autorização e seu valor
+  function approve(address spender, uint256 amount) public override returns(bool) {
+    _approve(msg.sender, spender, amount);
+
+    return true;
+  }
+
+  // função que vai aumentar o valor da autorização
+  function increaseAllowance(address spender, uint256 addedValue) public override returns(bool) {
+    _approve(msg.sender, spender, _allowances[msg.sender][spender] += addedValue);
+
+    return true;
+  }
+
+  // função que vai diminuir o valor da autorização
+  function decreaseAllowance(address spender, uint256 subtractedValue) public override returns(bool) {
+    require(_allowances[msg.sender][spender] >= subtractedValue, "ERC20: Valor a ser retinado maior que autorizado");
+
+    unchecked { _approve(msg.sender, spender, _allowances[msg.sender][spender] -= subtractedValue); }
+
+    return true;
+  }
+
+  // função que executar a autorização com seu valor
+  function _approve(address owner, address spender, uint256 amount) internal override {
+    _allowances[owner][spender] = amount;
+
+    emit Approval(owner, spender, amount);
+  }
+
+  function _mint(address account, uint256 amount) internal override {
+    _totalSupply += amount;
+    _balances[account] += amount;
+
+    emit Transfer(address(0), account, amount);
   }
 }
