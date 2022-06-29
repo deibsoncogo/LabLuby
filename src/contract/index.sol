848b2a8b06a0014dd46df98dd50d57a7f98ed9f0
Deibson Cogo <deibsoncogo@outlook.com>
2022-06-28 21:42:38 -0300

    feat: implementing tax transactions

--- src/contract/index.sol
+++ src/contract/index.sol
@@ -8,16 +8,18 @@ contract ProvaBlockchainLabLuby is ERC20 {
     // definindo variáveis
     address private owner;
     uint256 private supplyAvailable;
+    uint256 private taxPercentage;
 
     // definindo dicionário de informações
     mapping(address => bool) private addressVip;
 
     // o constructor irá criar a moeda e definir informação importantes
-    constructor(uint256 _supplyInitial, uint256 _supplyMax) ERC20("Deibson Lab Luby", "DLL") {
+    constructor(uint256 _supplyInitial, uint256 _supplyMax, uint256 _taxPercentage) ERC20("Deibson Lab Luby", "DLL") {
         require(_supplyInitial <= _supplyMax, "Valor inicial maior que o valor maximo");
 
         owner = msg.sender;
         supplyAvailable = _supplyMax - _supplyInitial;
+        taxPercentage = _taxPercentage;
 
         _mint(msg.sender, _supplyInitial);
     }
@@ -34,5 +36,10 @@ contract ProvaBlockchainLabLuby is ERC20 {
     // função que vai definir clientes vip
     function ToggleVip(address _account, bool _status) external {
         addressVip[_account] = _status;
+    }
+
+    // função que vai alterar a taxa das transações
+    function UpdateTax(uint256 _taxPercentage) external {
+        taxPercentage = _taxPercentage;
     }
 }
